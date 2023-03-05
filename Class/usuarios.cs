586e0491e941fd7662b81948bbc53b665a7533f0
alfonsouwu <107167883+alfonsouwu@users.noreply.github.com>
2023-03-04 20:08:02 -0600

    Ya muestra, inserta y modifica usuarios a BD

---
+++ Class/usuarios.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Class
+{
+    public class usuarios
+    {
+        public int id { get; set; }
+        public string usuario { get; set; } = "";
+        public string password { get; set; } = "";
+        public string nombre { get; set; } = "";
+        public string comentarios { get; set; } = "";
+        public bool activo_sn { get; set; }
+        public int id_entidad { get; set; }
+        public DateTime fecha_ultima_modificacion { get; set; }
+        public int usuario_ultima_modificacion { get; set; }
+        public DateTime fecha_alta_registro { get; set; }
+        public int usuario_alta_registro { get; set; }
+        public int id_persona { get; set; }
+    }
+}
--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using WebAPI.Resources;
 using System.Data;
 using Newtonsoft.Json;
-
+using WebAPI.Class;
 namespace WebAPI.Controllers
 {
     [ApiController]
@@ -12,15 +12,15 @@ namespace WebAPI.Controllers
         #region CRUD         
 
         [HttpGet(Name = "GetUsuarios")]
-        public dynamic ListaUsuarios()
+        public dynamic GetUsuarios(int id = 0)
         {
 
             List<Parametro> parametros = new List<Parametro>{
-                new Parametro("id", "0")
+                new Parametro("id", id.ToString())
             };
 
             DataTable dt = DataBase.Listar("sp_se_usuarios",parametros);
-            string json = JsonConvert.SerializeObject(dt);
+            string jsonUsuarios = JsonConvert.SerializeObject(dt);
 
             return new            
             {
@@ -28,11 +28,40 @@ namespace WebAPI.Controllers
                 message = "exito",
                 result = new
                 {
-                    usuarios = json 
+                    usuarios = JsonConvert.DeserializeObject<List<usuarios>>(jsonUsuarios) 
                 }
             };                
         }
 
+        [HttpPost(Name = "PutUsuario")]
+        public dynamic PutUsuario(string usuario , string password , int id_persona , string nombre, int id_entidad, int usuario_ultima_modificacion,int id = 0,string comentarios = "", bool activo_sn = true){
+            
+            List<Parametro> parametros = new List<Parametro>()
+            {
+                new Parametro("id", id.ToString()),
+                new Parametro("usuario", usuario),
+                new Parametro("password", password),
+                new Parametro("id_persona", id_persona.ToString()),
+                new Parametro("nombre", nombre),
+                new Parametro("comentarios", comentarios),
+                new Parametro("activo_sn", activo_sn.ToString()),
+                new Parametro("id_entidad", id_entidad.ToString()),
+                new Parametro("usuario_ultima_modificacion", usuario_ultima_modificacion.ToString())
+            };
+
+            DataTable dt = DataBase.Listar("sp_ui_usuario",parametros);
+            string jsonUsuarios = JsonConvert.SerializeObject(dt);
+
+            return new            
+            {
+                success = true,
+                message = "exito",
+                result = new
+                {
+                    usuarios = JsonConvert.DeserializeObject<List<usuarios>>(jsonUsuarios) 
+                }
+            };
+        }
         #endregion
     }
 }
